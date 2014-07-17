cb95dbc2172bfa5724167b7cc2e8d210bc5ef934
gangz <gangz2009@gmail.com>
2014-07-17 18:56:19 +0800

    more than third suite loan is forbidden

--- src/main/java/cn/agilean/demo/loan/LoanApplyDataFolder.java
+++ src/main/java/cn/agilean/demo/loan/LoanApplyDataFolder.java
@@ -2,13 +2,30 @@ package cn.agilean.demo.loan;
 
 public class LoanApplyDataFolder {
 	int years;
+	int suitesNum;
 	public void setLoanPeriod(int years) {
 		this.years = years;
 	}
 
 	public boolean approval() {
 		if (years>30) return false;
+		if (suitesNum>=3) return false;
 		return true;
+	}
+
+	public void setSuitesNum(int suitesNum) {
+		this.suitesNum = suitesNum;
+		
+	}
+
+	public void setTotalPrice(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setFirstPayment(int i) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/test/java/cn/agilean/demo/loan/TestEligibilityExamination.java
+++ src/test/java/cn/agilean/demo/loan/TestEligibilityExamination.java
@@ -2,13 +2,18 @@ package cn.agilean.demo.loan;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestEligibilityExamination {
+	LoanApplyDataFolder r;
+	@Before
+	public void setUp(){
+		r = new LoanApplyDataFolder();
+	}
 	@Test
 	public void LoanPeriodMoreThanThirtyYearShouldBeRejected()
 	{
-		LoanApplyDataFolder r = new LoanApplyDataFolder();
 		r.setLoanPeriod(31);
 		assertEquals(false,r.approval());
 	}
@@ -16,13 +21,23 @@ public class TestEligibilityExamination {
 	@Test
 	public void LoanPeriodLessOrEqualThirtyYearCouldBeAccepted()
 	{
-		LoanApplyDataFolder r = new LoanApplyDataFolder();
 		r.setLoanPeriod(30);
 		assertEquals(true,r.approval());
 	}
 	
-//	@Test
-//	public void 
-//	
+	@Test
+	public void ForbiddenThirdSuiteLoan()
+	{
+		r.setSuitesNum(3);
+		assertEquals(false, r.approval());
+	}
+	//@Test
+	public void FirstPaymentShouldReachThirtyPercentForFirstSuite()
+	{
+		r.setSuitesNum(1);
+		r.setTotalPrice(100);
+		r.setFirstPayment(30);
+		assertEquals(true, r.approval());
+	}
 
 }
