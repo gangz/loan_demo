e1fea016ddcf0259d518466ba036d83ea8cbcd26
gangz <gangz2009@gmail.com>
2014-07-18 10:31:28 +0800

    rename test of co-borrower for new co-borrower strategy

--- src/test/java/cn/agilean/demo/loan/TestEligibilityByCoBorrowerRelation.java
+++ src/test/java/cn/agilean/demo/loan/TestEligibilityByCoBorrowerRelation.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-public class TestEligibilityByCoBorrower {
+public class TestEligibilityByCoBorrowerRelation {
 	LoanApplyDataFolder loanDataFolder;
 	LoanEligibilityApproval eligibilityApproval;
 	ICreditService creditService;
