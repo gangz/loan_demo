1211868a008859cba83d8da6bc9b8d3117061d6b
gangz <gangz2009@gmail.com>
2014-07-18 16:57:47 +0800

    add amount calcuation based on not sufficient income

--- src/test/java/cn/agilean/demo/loan/repayment/TestLoanAmount.java
+++ src/test/java/cn/agilean/demo/loan/repayment/TestLoanAmount.java
@@ -8,37 +8,50 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import cn.agilean.demo.loan.Borrower;
-import cn.agilean.demo.loan.Gender;
 import cn.agilean.demo.loan.LoanApplyDataFolder;
 import cn.agilean.demo.loan.PersonID;
-import cn.agilean.demo.loan.eligibility.CreditQueryResult;
-import cn.agilean.demo.loan.eligibility.CreditService;
-import cn.agilean.demo.loan.eligibility.LoanEligibilityApproval;
-import cn.agilean.demo.loan.eligibility.strategies.CreditStrategy;
 
 public class TestLoanAmount {
-	@Test
-	public void LoanAmountEqualsApplyWhenIncomeIsSufficent()
-	{
-		LoanApplyDataFolder dataFolder =new LoanApplyDataFolder();
+	LoanApplyDataFolder dataFolder ;
+	Borrower primaryBorrower;
+	InterestsPolicy interestsPolicy;
+	LoanAmountApproval approval;
+	DateTimeService dateTimeService;
+	@Before
+	public void setUp(){
+		dataFolder =new LoanApplyDataFolder();
 		dataFolder.setTotalPrice (1300000);
 		dataFolder.setHouseBuildDate(new DateTime("2010-01-01"));
 		dataFolder.setFirstPayment(300000);
 		dataFolder.setLoanAppliedYears(20);
 		
-		Borrower primaryBorrower = new Borrower(new PersonID("310101200001010000"));
-		primaryBorrower.setMonthlyIncome(6435*3);
+		primaryBorrower = new Borrower(new PersonID("310101200001010000"));
+		
 		primaryBorrower.setSuitesNum(1);
-		
 		dataFolder.setPrimaryBorrower(primaryBorrower);
-		
-		InterestsPolicy interestsPolicy = new InterestsPolicy(4.7);
-		
-		DateTimeService dateTimeService = Mockito.mock(DateTimeService.class);
-		LoanAmountApproval approval = new LoanAmountApproval(dateTimeService);
+
+		interestsPolicy = new InterestsPolicy(4.7);
+		dateTimeService = Mockito.mock(DateTimeService.class);
+		approval = new LoanAmountApproval(dateTimeService);
 		approval.setInterestPolicy(interestsPolicy);
+		
+	}
+	@Test
+	public void LoanAmountEqualsApplyWhenIncomeIsSufficent()
+	{
+		primaryBorrower.setMonthlyIncome(6435*3);
 		Mockito.when(dateTimeService.now()).thenReturn(new DateTime("2010-01-01"));
 		assertEquals(1000000, approval.getAmount(dataFolder),0.01);
+	}
+
+	
+
+	@Test
+	public void LoanAmountEqualsApplyWhenIncomeIsNotSufficent()
+	{
+		primaryBorrower.setMonthlyIncome(6000*3);
+		Mockito.when(dateTimeService.now()).thenReturn(new DateTime("2010-01-01"));
+		assertEquals(932406.58, approval.getAmount(dataFolder),0.01);
 	}
 
 		
